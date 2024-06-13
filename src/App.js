0624cb61bfb20b2abe8a1264b0a2f3203e3e77dd
“Kamile.Paukste” <kamile.paukste1@gmail.com>
2024-06-13 15:31:10 +0300

    Set up changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
 }
 
 export default App;
