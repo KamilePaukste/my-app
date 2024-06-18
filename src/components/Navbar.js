e37e2c88ceb094bf0c666a113a8c036754d78e26
“Kamile.Paukste” <kamile.paukste1@gmail.com>
2024-06-18 14:03:59 +0300

    navbar and burger changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
       <Link to="/">
         <h1>Travel guide</h1>
       </Link>
-      <ul class="navmenu">
+      <ul class={click ? "navmenu active" : "navmenu"}>
         <li>
           <Link to="/">Apie mus</Link>
         </li>
@@ -28,9 +28,12 @@ const Navbar = () => {
           <Link to="/Susisiekite">Susisiekite</Link>
         </li>
       </ul>
-      <div className="hamburgermenu">
-        <FaTimes size={20} style={{ color: "white" }} />
-        <FaBars size={20} style={{ color: "white" }} />
+      <div className="hamburgermenu" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "white" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "white" }} />
+        )}
       </div>
     </div>
   );
